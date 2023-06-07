a8eb81a3c72670e24f779bf0d74f3773844ea6ec
Crowdbotics <team@crowdbotics.com>
2023-06-07 09:04:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ogt_drivers_41322/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ogt_drivers_41322;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ogt_drivers_41322";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ogt-drivers-41322",
+  url: "https://ogt-drivers-41322.botics.co",
+  api: "https://ogt-drivers-41322.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
